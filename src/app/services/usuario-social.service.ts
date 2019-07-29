5c9e8c4ecd224a92913899d3e204dfbc486a6a68
Lucas Parese <parese1995@gmail.com>
2019-07-28 21:03:21 -0300

    Commit en branch

---
+++ src/app/services/usuario-social.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
+import { Router } from '@angular/router';
+import { GuardService } from './guard.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  public tipoInicio: string;
+
+  usuario: any = {};
+
+  constructor(  private afAuth: AngularFireAuth,
+                private router: Router,
+                private guard: GuardService ) {
+                  
+    this.afAuth.authState.subscribe( user => {
+      
+      console.log( 'Estado del usuario: ', user );
+
+      if ( !user ) {
+        return;
+      }
+      this.usuario.nombre = user.displayName;
+      this.usuario.uid = user.uid;
+      this.usuario.email = user.email;
+      this.usuario.foto = user.photoURL;
+      this.usuario.token = user.refreshToken;
+      this.router.navigate(['/tabs']);
+      this.guard.guardarToken(this.usuario.token);
+    } );
+
+    this.guard.leerToken();
+
+  }
+
+   login(proveedor: string) {
+     if (proveedor==="google") {
+      this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+     } else {
+      this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+     }
+    
+   }
+
+   logout() {
+    console.log("Metodo logout FACEBOOK");
+    this.tipoInicio = undefined;
+    this.usuario.nombre = undefined;
+    this.usuario = {};
+    this.afAuth.auth.signOut();
+   }
+}
+
