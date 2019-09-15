e29c9ed60b6c2d155abf0720a5a22f531c70426b
Ricky Vadala <rickyvad_94@hotmail.com>
2019-09-15 13:38:09 -0300

    Follow y Unfollow terminado

---
+++ src/app/services/follow.service.ts
@@ -0,0 +1,105 @@
+import { Injectable } from '@angular/core';
+import { DataShareService } from './data-share.service';
+import { PerfilUsuarioModel } from '../models/perfil-usuario.model';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FollowService {
+  private urlABM = 'https://plogger-437eb.firebaseio.com';
+  usuario:PerfilUsuarioModel={};
+
+  constructor(private http: HttpClient,
+              private dataShare: DataShareService) { 
+                this.dataShare.currentUser.subscribe( usuario => this.usuario = usuario);
+              }
+
+  follow(keyOther){
+    // Obtiene los seguidos
+    return this.http.get(`${ this.urlABM }/perfil/${ this.usuario.key }/seguidos.json`)
+    .pipe(map((x:any) => {
+      //En esta parte cargo en seguidos del usuario que sigue
+      let seguidosArray: any[]; 
+      if (x!==null) {
+        //aca viene cuando ya sigue a otros usuarios
+        for (let index = 0; index < x.length; index++) {
+          if (x[index]===this.usuario.uid) {
+            return;
+          }
+        }
+        seguidosArray = x;
+        seguidosArray.push(keyOther);
+        return this.http.put(`${ this.urlABM }/perfil/${ this.usuario.key }/seguidos.json`,seguidosArray).subscribe(resp =>{
+          this.usuario.seguidos=seguidosArray;
+          this.dataShare.changeUser(this.usuario)
+          this.cargarEnSeguidores(keyOther);
+        });
+      } else {
+        //aca viene cuando es el primer usuario que sigue
+        seguidosArray = [keyOther]; 
+        return this.http.put(`${ this.urlABM }/perfil/${ this.usuario.key }/seguidos.json`,seguidosArray).subscribe(resp =>{
+          this.usuario.seguidos=seguidosArray;
+          this.dataShare.changeUser(this.usuario);
+          //En esta parte cargo en seguidores al usuario que se acaba de seguir
+          this.cargarEnSeguidores(keyOther).subscribe();
+        });
+      }
+    }));
+  } 
+
+  cargarEnSeguidores(keyOther) {
+    return this.http.get(`${ this.urlABM }/perfil/${ keyOther }/seguidores.json`)
+    .pipe(map((x:any) => {
+      let seguidoresArray: any[]; 
+      if (x!==null) {
+        //aca viene cuando ya es seguido por otros usuarios
+        for (let index = 0; index < x.length; index++) {
+          if (x[index]===this.usuario.uid) {
+            return;
+          }
+        }
+        seguidoresArray = x;
+        seguidoresArray.push(this.usuario.key);
+        return this.http.put(`${ this.urlABM }/perfil/${ keyOther}/seguidores.json`,seguidoresArray).subscribe();
+      } else {
+        //aca viene cuando es el primer usuario que sigue
+        seguidoresArray = [this.usuario.key]; 
+        return this.http.put(`${ this.urlABM }/perfil/${ keyOther}/seguidores.json`,seguidoresArray).subscribe();
+      }
+    }));
+  }
+
+  unfollow(keyOther) {
+    // Obtiene los seguidos
+    return this.http.get(`${ this.urlABM }/perfil/${ this.usuario.key }/seguidos.json`)
+    .pipe(map((x:any) => {
+      if (x!==null) {
+        for (let index = 0; index < x.length; index++) {
+          if (x[index]===keyOther) {
+            return this.http.delete(`${ this.urlABM }/perfil/${ this.usuario.key }/seguidos/${ index }.json`).subscribe(resp =>{
+              this.borrarDeSeguidores(keyOther).subscribe();
+            });
+          }
+        }
+      }
+    }));
+  }
+
+  borrarDeSeguidores(keyOther) {
+    // Obtiene los seguidos
+    return this.http.get(`${ this.urlABM }/perfil/${ keyOther }/seguidores.json`)
+    .pipe(map((x:any) => {
+      if (x!==null) {
+        for (let index = 0; index < x.length; index++) {
+          if (x[index]===this.usuario.key) {
+            return this.http.delete(`${ this.urlABM }/perfil/${ keyOther }/seguidores/${ index }.json`).subscribe();
+          }
+        }
+      }
+    }));
+  }
+
+
+}
