d91dc1bbe4c77cfabf987727e1a2ca343a66f903
Enzo Imola <enzoimola@MBP-de-Enzo.fibertel.com.ar>
2019-10-08 17:58:42 -0300

    Agrego estadisticas de eventos finalizados/en proceso

--- src/app/pages/reports/reports.page.ts
+++ src/app/pages/reports/reports.page.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { EventService } from 'src/app/services/event.service';
 import { EventoModel } from 'src/app/models/evento.model';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
-import { Label, SingleDataSet } from 'ng2-charts';
+import { Label, SingleDataSet, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
 import { UsuarioPloggerService } from 'src/app/services/usuario-plogger.service';
+import { EventTypePageModule } from '../event-type/event-type.module';
 
 @Component({
   selector: 'app-reports',
@@ -22,6 +23,16 @@ export class ReportsPage implements OnInit {
   public pieChartType: ChartType = 'pie';
   public pieChartLegend = true;
   public pieChartPlugins = [];
+
+  //Estadisticas eventos finalizados 
+  public pieChartOptionsEventosFinal: ChartOptions = {
+    responsive: true
+  };
+  public pieChartLabelsEventosFinal: Label[] = [];
+  public pieChartDataEventosFinal: SingleDataSet = [];
+  public pieChartTypeEventosFinal: ChartType = 'pie';
+  public pieChartLegendEventosFinal = true;
+  public pieChartPluginsEventosFinal = [];
 
   //Estadistica de ciudades  
   view: any[] = [360, 300];
@@ -45,6 +56,7 @@ export class ReportsPage implements OnInit {
   contCordoba = 0;
   contBsAs = 0;
   mostrarSexo = false;
+  mostrarEventosFinalizados = false;
   usuarios: any[] = [];
   sexoUsuario: any[] = [];
 
@@ -54,13 +66,21 @@ export class ReportsPage implements OnInit {
   contFemenino = 0;
   contPlanta = 0;
 
+  //Creadores de eventos
+  contEventoFinalizado =0;
+  contEventoEnProceso =0;
+
 
   constructor(private eventServices: EventService,
-    private usuarioPloggerService: UsuarioPloggerService) { }
+    private usuarioPloggerService: UsuarioPloggerService) { 
+      monkeyPatchChartJsTooltip();
+      monkeyPatchChartJsLegend();
+    }
 
   ngOnInit() {
     this.eventosPorCiudades();
     this.personasSegunSexo();
+    this.usuariosPorEventoCreado();
   }
 
   personasSegunSexo() {
@@ -90,8 +110,6 @@ export class ReportsPage implements OnInit {
       this.mostrarSexo = true;
     })
   }
-
-
 
 
   eventosPorCiudades() {
@@ -132,7 +150,23 @@ export class ReportsPage implements OnInit {
   }
 
 
+  usuariosPorEventoCreado() {
+    let fechaActual = new Date; 
+    this.eventServices.obtenerEventos().subscribe(resp => {
+      this.eventos = resp; 
+      this.eventos.forEach(evento => {
+     let fechaFinEvento = evento.endDate;
+        if (fechaFinEvento< fechaActual.toISOString()){
+          this.contEventoFinalizado = this.contEventoFinalizado + 1;
+        }else {
+          this.contEventoEnProceso = this.contEventoEnProceso + 1;
+        }
+      });
+      // Datos 
+      this.pieChartLabelsEventosFinal = ['Eventos finalizados', 'Eventos en proceso'];
+      this.pieChartDataEventosFinal = [this.contEventoFinalizado, this.contEventoEnProceso];
+      this.mostrarEventosFinalizados = true;
+    })
 
-
-
+  }
 }
