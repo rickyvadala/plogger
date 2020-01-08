767d037c17718081c95fc000967c502397eef093
Enzo Imola <enzoimola@MBP-de-Enzo.fibertel.com.ar>
2020-01-08 16:09:44 -0300

    Cambio de email

--- src/app/pages/modal-login/modal-login.page.ts
+++ src/app/pages/modal-login/modal-login.page.ts
@@ -137,7 +137,7 @@ export class ModalLoginPage implements OnInit {
   ngOnInit() {
     this.usuario = new UsuarioPloggerModel();
     this.usuario = {
-      email: 'coco1@hotmail.com',
+      email: 'enzo66@hotmail.com',
       password: '12345678'
     };
   }
